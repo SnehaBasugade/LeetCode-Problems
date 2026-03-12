298508546f050803fba1fe2c187f19b9c73a2b4a
SnehaBasugade <snehabasugade1111@gmail.com>
2026-03-12 06:28:29 -0700

    45. Jump Game II
    You are given a 0-indexed array of integers nums of length n. You are initially positioned at index 0.
    
    Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at index i, you can jump to any index (i + j) where:
    
    0 <= j <= nums[i] and
    i + j < n
    Return the minimum number of jumps to reach index n - 1. The test cases are generated such that you can reach index n - 1.
    
     
    
    Example 1:
    
    Input: nums = [2,3,1,1,4]
    Output: 2
    Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
    Example 2:
    
    Input: nums = [2,3,0,1,4]
    Output: 2
    
    Intuition :
    We have to find the minimum number of jumps required to reach the end of a given array of non-negative integers i.e the shortest number of jumps needed to reach the end of an array of numbers.
    Explanation to Approach :
    We are using a search algorithm that works by moving forward in steps and counting each step as a jump.
    The algorithm keeps track of the farthest reachable position at each step and updates the number of jumps needed to reach that farthest position.
    The algorithm returns the minimum number of jumps needed to reach the end of the array.
    Complexity :
    Time complexity : O(n)
    Space complexity: O(1)

---
+++ DAY=130.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def jump(self, nums):
+        ans=0
+        end=0
+        farthest=0
+
+        for i in range(len(nums) - 1):
+            farthest=max(farthest,i+nums[i])
+            if farthest >= len(nums) - 1:
+                ans +=1
+                break
+            if i == end:
+                ans += 1
+                end=farthest
+
+        return ans    
